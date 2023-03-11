6c9cee09c7c49cf6774844474c2174fa9dad6eac
vasenoi <dok18263@gmail.com>
2023-03-12 01:27:26 +0500

    airports-search-v1.1 Изменил проверку входных аргументов

--- src/main/java/com/vasenoi/Main.java
+++ src/main/java/com/vasenoi/Main.java
@@ -8,38 +8,60 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Main {
+
+    static int retupe_nColumn() {
+        while (true) {
+            System.out.print("Введите корректный номер колонки: ");
+            try {
+                String input = new BufferedReader(new InputStreamReader(System.in)).readLine();
+                return Integer.valueOf(input)-1;
+            } catch (Exception e) {
+
+            }
+        }
+    }
+
     public static void main(String[] args) throws IOException {
-        int nColumn;
+        // параметры по умолчанию
+        int nColumn = 2;
+        String path = "airports.csv";
+
         try {
+            // при отсутствии переданных аргументов программа запрашивает ввод номера колонки
             // первый аргумент переданый при запуске программы принимается за номер столбца
-            nColumn = Integer.valueOf(args[0])-1;
+            // второй аргумент переданный при вызове программы принимается за путь до файла
+            switch (args.length) {
+                case 0:
+                    nColumn = retupe_nColumn();
+                    break;
+                case 1:
+                    try {
+                        nColumn = Integer.valueOf(args[0])-1;
+                    } catch (Exception e) {
+                        nColumn = retupe_nColumn();
+                        break;
+                    }
+                    break;
+                default:
+                    nColumn = Integer.valueOf(args[0])-1;
+                    path = args[1];
+                    break;
+            }
         } catch (Exception e) {
-            // в случае если при запуске программы передан не корректный аргумент запрашивается ввод номера столбца
-            System.out.print("Введите номер колонки: ");
-            String search = new BufferedReader(new InputStreamReader(System.in)).readLine().toLowerCase();
-            nColumn = Integer.valueOf(search)-1;
+            System.out.println(e);
         }
 
-        String line = "";
-        String splitBy = ",";
-        String path = "airports.csv";
         String search = "";
         long time;
         ArrayList<String> column = new ArrayList<String>();
         ArrayList<String> tableRows = new ArrayList<String>();
 
 
-        // второй аргумент переданный при вызове программы принимается за путь до файла
-        try {
-            if (args.length == 2) path = args[1];
-        } catch (Exception e) {
-            path = "airports.csv";
-        }
-
-
-        // Парсинг .csv файла
         try
         {
+            // Парсинг .csv файла
+            String line = "";
+            String splitBy = ",";
             time = System.currentTimeMillis();
             BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
             while ((line = bufferedReader.readLine()) != null)
@@ -49,32 +71,35 @@ public class Main {
                 column.add(row[nColumn]);
             }
             System.out.println("Парсинг занял " + (System.currentTimeMillis()-time) + " мс");
+
+
+            // Поиск
+            while (true) {
+                ArrayList<String> searchResult = new ArrayList<String>();
+
+                // Ввод искомой строки
+                System.out.print("Введите текст для поиска совпадений: ");
+                search = new BufferedReader(new InputStreamReader(System.in)).readLine().toLowerCase();
+                if (search.equals("!quit")) System.exit(0);
+
+                // Поиск нужных элементов
+                time = System.currentTimeMillis();
+                for(int i = 0; i<column.size(); i++){
+                    if (column.get(i).toLowerCase().contains(search)) searchResult.add(column.get(i)+"["+tableRows.get(i)+"]");
+                }
+
+                // Сортировка найденных строк
+                Collections.sort(searchResult);
+
+                // Вывод найденных строк
+                for(String a : searchResult) System.out.println(a);
+                time = System.currentTimeMillis()-time;
+                System.out.println("Количество найденных строк: "+searchResult.size()+" Время затраченое на поиск: " + time + " мс");
+            }
         }
         catch (IOException e)
         {
             e.printStackTrace();
-        }
-
-        // Поиск
-        while (true) {
-            ArrayList<String> searchResult = new ArrayList<String>();
-
-            // Ввод искомой строки
-            System.out.print("Введите текст для поиска совпадений: ");
-            search = new BufferedReader(new InputStreamReader(System.in)).readLine().toLowerCase();
-            if (search.equals("!quit")) System.exit(0);
-
-            // Поиск нужных элементов
-            time = System.currentTimeMillis();
-            for(int i = 0; i<column.size(); i++){
-                if (column.get(i).toLowerCase().contains(search)) searchResult.add(column.get(i)+"["+tableRows.get(i)+"]");
-            }
-            // Сортировка найденных строк
-            Collections.sort(searchResult);
-            // Вывод найденных строк
-            for(String a : searchResult) System.out.println(a);
-            time = System.currentTimeMillis()-time;
-            System.out.println("Количество найденных строк: "+searchResult.size()+" Время затраченое на поиск: " + time + " мс");
         }
     }
 }
